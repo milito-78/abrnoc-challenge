4664f806b9fd7b3e2bbbc93a098ef673ae6aa13d
Milito <milad.purahmad78@gmail.com>
2024-08-20 12:09:29 +0330

    [INITIAL] : Initial Commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.User = void 0;
+var console = require("node:console");
+var User = /** @class */ (function () {
+    function User() {
+    }
+    return User;
+}());
+exports.User = User;
+console.log(new User());
