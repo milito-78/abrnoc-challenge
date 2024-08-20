8ad9fa1a8cbd475547af9d2f6fccb540e0179656
Milito <milad.purahmad78@gmail.com>
2024-08-20 12:09:29 +0330

    [INITIAL] : Initial Commit

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+export class User {
+  name: string;
+  age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const z = new User("11",12);
+
+console.log(z)
